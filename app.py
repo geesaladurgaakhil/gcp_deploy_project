d104c252716fd2424de23b2ce06e65848ba1e0c0
geesaladurgaakhil <56544604+geesaladurgaakhil@users.noreply.github.com>
2025-11-08 22:47:50 +0530

    code change pushed

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app  = Flask(__name__)
 @app.route("/")
 def hello():
     name = os.getenv("NAME", "World")
-    return f"Hello {name} to first python app!!"
+    return f"Hello {name} to first python app,, CICD successfull with github actions!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
